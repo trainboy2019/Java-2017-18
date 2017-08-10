5fb760196b6f53323aa5ebe7645c9cc783b105ac
trainboy2019 <ikerolader@gmail.com>
2017-08-10 12:48:52 -0400

    Chapter 1

---
+++ Java2017-18/src/Chapter1.java
@@ -0,0 +1,54 @@
+
+public class Chapter1 {
+	public static void main(String[] args) {
+		//1.3
+		System.out.println("    J     A    V     V   A");
+		System.out.println("    J    A A    V   V   A A");
+		System.out.println("J   J   AAAAA    V V   AAAAA");
+		System.out.println(" J J   A     A    V   A     A");
+		
+		//1.5
+		System.out.println(((9.5*4.5)-(2.5*3))/(45.5-3.5));
+		
+		//1.8
+		System.out.println(2*5.5*(1.0-(1/3)+(1/5)-(1/7)+(1/9)-(1/11)+(1/13)));
+		System.out.println(5.5*5.5*(1.0-(1/3)+(1/5)-(1/7)+(1/9)-(1/11)+(1/13)));
+		
+		//1.9
+		System.out.println(4.5*7.9);
+		
+		//1.10
+		System.out.println(((14/45.5)*60)*0.6);
+		
+		//1.11
+		System.out.println(312032486+(31536000/7)-(31536000/13)+(31536000/45));
+		System.out.println(312032486+((31536000/7)-(31536000/13)+(31536000/45))*2);
+		System.out.println(312032486+((31536000/7)-(31536000/13)+(31536000/45))*3);
+		System.out.println(312032486+((31536000/7)-(31536000/13)+(31536000/45))*4);
+		System.out.println(312032486+((31536000/7)-(31536000/13)+(31536000/45))*5);
+		
+		//1.12
+		System.out.println(((24*1.6)/100.35)*60);
+		
+		//1.13
+		double a=3.4;
+		double b=50.2;
+		double c=2.1;
+		double d=0.55;
+		double e;
+		double f;
+		double x;
+		double y;
+		e=a+b;
+		f=c+d;
+		x=((e*d)-(b*f))/((a*d)-(b*c));
+		y=((a*f)-(e*c))/((a*d)-(b*c));
+		System.out.println(x);
+		System.out.println(y);
+		
+		
+		
+		
+	}
+
+}
